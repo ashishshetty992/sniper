0ae05fb3333911774d6b0aeaf2dd7d8ece7f11fe
Kanika Shetty <kanishetty@yahoo.com>
2023-12-18 08:09:39 +0000

    added changes related to pyinstaller

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://sniper:password@localhost/sniper"
+DATABASE_URL = "mysql+mysqlconnector://sniper:password@192.168.0.107/sniper"
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import uvicorn
+import multiprocessing
+from app import main
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     uvicorn.run("app.main:app", host="localhost", port=9001, reload=True)
