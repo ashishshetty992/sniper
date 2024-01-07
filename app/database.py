f7e16e6cdc273bf46dc7a353de0590671b7ade84
prithviraj k <kanishetty@yahoo.com>
2024-01-07 16:57:42 +0530

    made databses path director changes

--- app/database.py
+++ app/database.py
@@ -2,12 +2,18 @@ from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import DeclarativeBase
 from sqlalchemy.ext.declarative import declarative_base
 import os
+import sys
 from dotenv import load_dotenv
 
 from sqlalchemy.orm import sessionmaker
 
-dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-load_dotenv(dir_path+"/setup/setup.env")
+if getattr(sys, 'frozen', False):
+    dir_path = sys._MEIPASS + '/setup.env'
+else:
+    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+"/setup/setup.env"
+
+print(dir_path)
+load_dotenv(dir_path)
 
 # DATABASE_URL = "mysql+mysqlconnector://sniper:password@192.168.0.100/sniper"
 # DATABASE_URL = "mysql+mysqlconnector://shiksha:Shiksha123@172.31.5.1/sniper_new"
