dc1acf9a3ed0954db69b8a99282ddc0f6b261c5a
prithviraj k <kanishetty@yahoo.com>
2024-01-07 16:14:26 +0530

    fetching db creds from env

--- app/database.py
+++ app/database.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import DeclarativeBase
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
 from sqlalchemy.orm import sessionmaker
 
+dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+load_dotenv(dir_path+"/setup/setup.env")
+
 # DATABASE_URL = "mysql+mysqlconnector://sniper:password@192.168.0.100/sniper"
 # DATABASE_URL = "mysql+mysqlconnector://shiksha:Shiksha123@172.31.5.1/sniper_new"
-DATABASE_URL = "mysql://avnadmin:AVNS_SLGLj-oA7Vkwm222SyW@sniper-ashishshetty992-5e41.a.aivencloud.com:14764/sniper_new"
-
+DATABASE_URL = f"mysql://{os.getenv("MYSQL_USER_NAME")}:{os.getenv("MYSQL_USER_PASSWORD")}@localhost/{os.getenv("MYSQL_DATABASE")}"
+print("DATABASE_URL-->", DATABASE_URL)
 
 
 engine = create_engine(DATABASE_URL)
